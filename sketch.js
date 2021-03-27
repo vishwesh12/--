60436a50698466c5acea9e19bb19dd25fb5ee1d8
vishwesh12 <vishweshshukla95@gmail.com>
2021-03-27 04:15:21 -0700

    platform added

--- sketch.js
+++ sketch.js
@@ -16,8 +16,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
-
+    ground = new Ground(600,height,1200,20);
+    platform=new Ground(150,300,300,170);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -57,6 +57,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+    platform.display();
 
     bird.display();
 }
